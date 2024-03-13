92244cadc8b9fbdf06441e6ecaf78b260d5e6214
Badal <badal.chowdhary@iitgn.ac.in>
2024-03-13 20:15:10 +0530

    initial

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=> console.log("DB Connected"))
+.catch((err)=>{
+    console.log(err)
+});
+
+//Enable CORS for all routes
+app.use(cors());
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Backend Server is running !");
+});
